dea900d79b34386c8eca75adde7796d62eaf2a6b
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-11 19:06:50 +0200

    use another image

--- src/App.js
+++ src/App.js
@@ -93,22 +93,26 @@ async function fetchOnePokemon(id) {
     console.log(`---fetch ${id} done`)
     const data = await response.json();
     console.log(`---fetch ${id} data `, data.id)
+
+    function getImage() {
+        return data.sprites.other["official-artwork"].front_default;
+    }
+
     return {
         id: String(data.id),
         name: data.name,
-        image: data.sprites.other.dream_world.front_default,
+        image: getImage(),
         types: data.types.map(t => t.type.name).join(", ")
     };
 }
 
-
-function getIdFromUrl(url) {
-    const urlParts = url.split("/");
-    return urlParts[urlParts.length - 2];
-
-}
-
 async function fetchAllPokemon() {
+    function getIdFromUrl(url) {
+        const urlParts = url.split("/");
+        return urlParts[urlParts.length - 2];
+
+    }
+
     console.log(`---fetch all `)
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=-1`);
     console.log(`---fetch all done`)
