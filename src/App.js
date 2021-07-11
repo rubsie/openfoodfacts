2e9a91e698b6085f1a6e44f777f0d282376fd2f4
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-11 21:13:56 +0200

    refactor: extract context

--- src/App.js
+++ src/App.js
@@ -1,50 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {Container, Row} from "react-bootstrap";
 import {ListAllPokemons} from "./components/listAllPokemons";
-import {fromLocalStorage, toLocalStorage} from "./utilities/localStorage";
 import {Pokemon} from "./components/pokemon";
-import {fetchOnePokemon} from "./utilities/fetch";
 import {ShownPokemonsProvider, useShownPokemonsContext} from "./contexts/shownPokemonsContext";
 
 
 function ProvidedApp() {
-    const {shownPokemonIds, setShownPokemonIds} = useShownPokemonsContext();
-    const [pokemons, setPokemons] = useState([]);
+    const {shownPokemonIds, shownPokemonsData, addPokemon, removePokemon} = useShownPokemonsContext();
     const [clickedPokemon, setClickedPokemon] = useState();
 
-    useEffect(() => {
-        async function fetchShownPokemons() {
-            toLocalStorage(shownPokemonIds);
-            const idsToFetch = shownPokemonIds.filter(id => !pokemons.find(p => p.id === id));
-            console.log(`fetchPokemon `, {idsToFetch});
-            if (!idsToFetch.length) return;
-
-            const fetchedData = await Promise.all(idsToFetch.map(id => fetchOnePokemon(id)));
-            console.log({fetchedData});
-            setPokemons([...pokemons, ...fetchedData]);
-        }
-
-        fetchShownPokemons();
-    }, [shownPokemonIds, pokemons]);
-
-
-    function addPokemon(id) {
-        console.log(`add ${id}`);
-        if (!shownPokemonIds.includes(id))
-            setShownPokemonIds([...shownPokemonIds, id].sort((a, b) => Number(a) - Number(b)));
-    }
-
-    function removePokemon(id) {
-        console.log(`remove ${id}`);
-        if (shownPokemonIds.includes(id))
-            setShownPokemonIds(shownPokemonIds.filter(i => i !== id));
-    }
-
-    console.log({shownPokemonIds});
-    console.log({pokemons});
-    if (!pokemons) return null;
     return (<div>
             <Container fluid className="mt-3 mb-3">
                 <h1>My Pokemons</h1>
@@ -57,7 +23,7 @@ function ProvidedApp() {
                 </Row>
                 <Row>
                     {shownPokemonIds.map(id => {
-                        const pokemon = pokemons.find(p => p.id === id);
+                        const pokemon = shownPokemonsData.find(p => p.id === id);
                         return <Pokemon key={id} pokemon={pokemon}
                                         setSelectedPokemon={() => setClickedPokemon(pokemon)}/>
                     })}
--- src/contexts/shownPokemonsContext.js
+++ src/contexts/shownPokemonsContext.js
@@ -1,14 +1,53 @@
 import React, {createContext, useCallback, useContext, useEffect, useMemo, useState} from 'react';
-import {fromLocalStorage} from "../utilities/localStorage";
+import {fromLocalStorage, toLocalStorage} from "../utilities/localStorage";
+import {fetchOnePokemon} from "../utilities/fetch";
 
 
 const ShownPokemonsContext = createContext();
 
 export function ShownPokemonsProvider(props) {
     const [shownPokemonIds, setShownPokemonIds] = useState(() => fromLocalStorage());
+    const [shownPokemonsData, setShownPokemonsData] = useState([]);
 
-    const api = useMemo(() => ({shownPokemonIds, setShownPokemonIds}),
-        [shownPokemonIds, setShownPokemonIds]);
+    console.log({shownPokemonIds});
+    console.log({shownPokemonsData});
+
+    useEffect(() => {
+        async function fetchShownPokemons() {
+            toLocalStorage(shownPokemonIds);
+            const idsToFetch = shownPokemonIds.filter(id => !shownPokemonsData.find(p => p.id === id));
+            console.log(`fetchPokemon `, {idsToFetch});
+            if (!idsToFetch.length) return;
+
+            const fetchedData = await Promise.all(idsToFetch.map(id => fetchOnePokemon(id)));
+            console.log({fetchedData});
+            setShownPokemonsData([...shownPokemonsData, ...fetchedData]);
+        }
+
+        fetchShownPokemons();
+    }, [shownPokemonIds, shownPokemonsData]);
+
+    const addPokemon = useCallback(id => {
+        console.log(`add ${id}`);
+        if (!shownPokemonIds.includes(id))
+            setShownPokemonIds([...shownPokemonIds, id].sort((a, b) => Number(a) - Number(b)));
+    }, [shownPokemonIds, setShownPokemonIds]);
+
+    const removePokemon = useCallback(id => {
+        console.log(`remove ${id}`);
+        if (shownPokemonIds.includes(id))
+            setShownPokemonIds(shownPokemonIds.filter(i => i !== id));
+    }, [shownPokemonIds, setShownPokemonIds]);
+
+    const api = useMemo(() => ({
+            shownPokemonIds,
+            setShownPokemonIds,
+            shownPokemonsData,
+            setShownPokemonsData,
+            addPokemon,
+            removePokemon
+        }),
+        [shownPokemonIds, setShownPokemonIds, shownPokemonsData, setShownPokemonsData, addPokemon, removePokemon]);
 
     return <ShownPokemonsContext.Provider value={api}>
         {props.children}
