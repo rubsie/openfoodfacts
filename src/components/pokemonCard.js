5ae9150b2f9d0774004c09d14b8383ea200c1cd2
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2021-07-13 14:04:02 +0200

    fix sizing on iphone

--- src/components/pokemonCard.js
+++ src/components/pokemonCard.js
@@ -4,13 +4,13 @@ export function PokemonCard(props) {
     const {pokemon, setSelectedPokemon} = props;
 
     if (!pokemon) return null;
-    return <MDBCol size={12} sm={4} md={3} xl={2} className="mt-3" onClick={setSelectedPokemon}>
+    return <MDBCol size={6} sm={4} md={3} xl={2} className="mt-3" onClick={setSelectedPokemon}>
         <MDBCard className="h-100">
             <MDBCardBody className="text-center p-1 mb-3">
                 <MDBCardTitle>{pokemon.id}. {pokemon.name}</MDBCardTitle>
                 <MDBCardText>Type: {pokemon.types}</MDBCardText>
+            <MDBCardImage src={pokemon.image} alt={pokemon.name} variant="bottom" className="p-0 p-sm-2 p-md-2 w-100 h-auto"/>
             </MDBCardBody>
-            <MDBCardImage src={pokemon.image} alt={pokemon.name} variant="bottom" className="p-5 p-sm-2 p-md-2"/>
         </MDBCard>
     </MDBCol>
 }
